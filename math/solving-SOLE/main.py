bd78ede73f50c762a61d3260ff7413f6e0c123cc
A.Mogilevskiy <a.mogilevskiy@tinkoff.ru>
2020-08-27 17:19:33 +0300

    (math) oksana_help

--- math/solving-SOLE/main.py
+++ math/solving-SOLE/main.py
@@ -1,3 +1,112 @@
+# # from copy import deepcopy
+
+# # def transform_matrix_to_reduced_row_echelon_form(A_input):
+# #     A = deepcopy(A_input)
+
+# #     row_count = len(A)
+# #     column_count = len(A[0])
+    
+# #     lead = 0
+
+# #     for row_index in range(row_count):
+# #         if lead >= column_count:
+# #             return
+# #         i = row_index
+
+# #         while A[i][lead] == 0:
+# #             i += 1
+# #             if i == row_count:
+# #                 i = row_index
+# #                 lead += 1
+
+# #                 if column_count == lead:
+# #                     return
+        
+# #         A[i], A[row_index] = A[row_index], A[i]
+
+# #         current_item = A[row_index][lead]
+# #         A[row_index] = [item / float(current_item) for item in A[row_index]]
+
+# #         for i in range(row_count):
+# #             if i != row_index:
+# #                 current_item = A[i][lead]
+# #                 A[i] = [iv - current_item * rv for rv, iv in zip(A[row_index], A[i])]
+
+# #         lead += 1
+# #     return A
+
+# # def solve_SOLE_by_gaussian_elimination(A, B):
+# #     """
+# #     todo: write description
+# #     return vector
+# #     """
+# #     A_rref = transform_matrix_to_reduced_row_echelon_form(A)
+
+# #     print(A_rref)
+
+# #     result = []
+    
+# #     cursor = len(A_rref[0])
+
+# #     # for B_index in reversed(range(len(B))):
+# #     #     factor_x = A_rref[B_index][cursor]
+# #     #     x = B[B_index] / float(factor_x)
+
+# # mtx = [
+# #    [ 1, 2, 4],
+# #    [3, 2, 2],
+# #    [3, 1, 2,]
+# # ]
+# # b = [2, 4]
+ 
+# # result = solve_SOLE_by_gaussian_elimination(mtx, b)
+
+# def ToReducedRowEchelonForm( M):
+#     if not M: return
+#     lead = 0
+#     rowCount = len(M)
+#     columnCount = len(M[0])
+#     for r in range(rowCount):
+#         if lead >= columnCount:
+#             return
+#         i = r
+#         while M[i][lead] == 0:
+#             i += 1
+#             if i == rowCount:
+#                 i = r
+#                 lead += 1
+#                 if columnCount == lead:
+#                     return
+#         M[i],M[r] = M[r],M[i]
+#         lv = M[r][lead]
+#         M[r] = [ mrx / float(lv) for mrx in M[r]]
+#         for i in range(rowCount):
+#             if i != r:
+#                 lv = M[i][lead]
+#                 M[i] = [ iv - lv*rv for rv,iv in zip(M[r],M[i])]
+#         lead += 1
+
+# mtx1 = [
+#    [ 1, 2, 8],
+#    [3, 2, 2],
+#    [3, 1, 2,]
+# ]
+# print('------')
+# ToReducedRowEchelonForm( mtx1 )
+# for rw in mtx1:
+#   print('; '.join( (str(rv) for rv in rw) ))
+
+# todo: new version
+
+# --- исходные данные
+# myA = [
+#     [1.0, -2.0, 3.0, -4.0],
+#     [3.0, 3.0, -5.0, -1.0],
+#     [3.0, 0.0, 3.0, -10.0],
+#     [-2.0, 1.0, 2.0, -3.0]
+# ]
+# myB = [2.0, -3.0, 8.0, 5.0]
+
 A = [
     [1.0, 2.0, 4.0],
     [3.0, 2.0, 2.0],
