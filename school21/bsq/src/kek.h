a6a33c9bb8dc9c188daceb09d88bc5dc02d4ce1a
Casey Deon <cdeon@ox-c5.msk.21-school.ru>
2020-09-27 20:47:58 +0300

    (sh21-bsq) init proj

---
+++ school21/bsq/src/kek.h
@@ -0,0 +1,6 @@
+#ifndef KEK_H
+# define KEK_H
+
+void ft_print_table(char **table, int size);
+
+#endif
---
+++ school21/bsq/src/utils.h
@@ -0,0 +1,8 @@
+#ifndef UTILS_H
+# define UTILS_H
+
+# include <unistd.h>
+
+void ft_putstr(char *str);
+
+#endif
