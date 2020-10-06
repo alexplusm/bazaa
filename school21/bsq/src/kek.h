75a99f00eb5578b65f983a19470ee2b142e25c5c
A.Mogilevskiy <a.mogilevskiy@tinkoff.ru>
2020-10-06 11:02:17 +0300

    refactor (sh21) bsq dir

--- school21/bsq/src/kek.h
+++
@@ -1,6 +0,0 @@
-#ifndef KEK_H
-# define KEK_H
-
-void ft_print_table(char **table, int size);
-
-#endif
--- school21/bsq/src/utils.h
+++
@@ -1,8 +0,0 @@
-#ifndef UTILS_H
-# define UTILS_H
-
-# include <unistd.h>
-
-void ft_putstr(char *str);
-
-#endif
