2991eaf4eebfb6225fa994ffa301a6c6c5d444cb
A.Mogilevskiy <a.mogilevskiy@tinkoff.ru>
2020-10-20 20:02:34 +0300

    (go-db) file uploading controller

--- projects/go-db/TODO.md
+++ projects/go-db/TODO.md
@@ -1,1 +1,4 @@
 * if I change package code and dont run "go build" what happens? "go run main.go"
+
+? Если будет несколько раз загружать один и тот же архив?
+? Одинаковые фотки в разных архивах?
--- projects/go-db/src/main.go
+++ projects/go-db/src/main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"github.com/Alexplusm/bazaa/projects/go-db/src/controllers"
 	"github.com/Alexplusm/bazaa/projects/go-db/src/dbcon"
+	"github.com/labstack/echo"
 )
 
 func main() {
 	dbcon.Connect()
+
+	e := echo.New()
+
+	e.POST("/upload/images", controllers.UploadFiles)
+
+	// todo: PORT from .env
+	e.Logger.Fatal(e.Start(":1234"))
 }
