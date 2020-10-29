9e0b16a193df6e55c7834d3aa1dfc96f65f8bf74
A.Mogilevskiy <a.mogilevskiy@tinkoff.ru>
2020-10-29 16:46:33 +0300

    feat (s21-libft) init ft_memset

--- school21/42cursus/libft/ft_memset.c
+++ school21/42cursus/libft/ft_memset.c
@@ -1,1 +1,18 @@
-//
+#include <stdio.h>
+
+void *tf_memset(void *b, int c, int len)
+{
+    // int i;
+
+    // // todo: while
+    // for (i = 0; i < len; i++) {
+    //     ((int *)b)[i] = c;
+    //     printf("i: %d | value: %c | char: %c \n", i, ((char *)b)[i], c);
+    // }
+
+    // return b;
+    unsigned char *ptr = b;
+    while (len-- > 0)
+        *ptr++ = c;
+    return b;
+}
