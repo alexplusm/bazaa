41853fb65bc594660a56c962af66565314c0e05a
A.Mogilevskiy <a.mogilevskiy@tinkoff.ru>
2020-11-02 19:46:27 +0300

    test (go-db_zip_parsing) create game

--- projects/go-db/src/controllers/create_game_test.go
+++ projects/go-db/src/controllers/create_game_test.go
@@ -1,40 +1,55 @@
 package controllers
 
 import (
+test	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"strings"
 	"testing"
 
+	"github.com/jackc/pgx/v4/pgxpool"
 	"github.com/labstack/echo"
 	"github.com/stretchr/testify/assert"
-
-	// "github.com/DATA-DOG/go-sqlmock" // todo: remove from all (.mod and .sum and others)
-
-	"github.com/jackc/pgx/v4/pgxpool"
 )
 
-var (
-	JSON = `{"name":"Jon Snow","email":"jon@labstack.com"}`
-)
+func getTestPool() *pgxpool.Pool {
+	p := new(pgxpool.Pool)
+
+	// todo: docker test
+	// conn, err := dbcon.Connect()
+	// if err != nil {
+	// 	// logger
+	// 	fmt.Printf("Unable to connection to database: %v\n", err)
+	// }
+	// defer conn.Close()
+	// fmt.Println("Connected to database!")
+
+	return p
+}
 
 func TestCreateGame(t *testing.T) {
+	var (
+		data = `{
+			"name": "new game",
+			"answer_type": 1,
+			"start_date": "2020-11-02T21:33:35.298Z",
+			"end_date": "2020-11-02T21:33:35.299Z",
+			"question": "Выберите правильный ответ",
+			"options": "yep, nope"
+		}`
+	)
+
 	e := echo.New()
-	req := httptest.NewRequest(http.MethodPost, "/", strings.NewReader(JSON))
+	req := httptest.NewRequest(http.MethodPost, "/", strings.NewReader(data))
 	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
 	rec := httptest.NewRecorder()
 	ctx := e.NewContext(req, rec)
 
-	p := new(pgxpool.Pool)
-
+	p := getTestPool()
 	handler := CreateGame(p)
 
-	handler(ctx)
-
 	if assert.NoError(t, handler(ctx)) {
+		fmt.Println()
 		assert.Equal(t, http.StatusOK, rec.Code)
 	}
-
-	// todo
-	// echo.MIMEApplicationJSON
 }
