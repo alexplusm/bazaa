840336d0b612fc1920ca81af4b9ad059e1c3ffb3
A.Mogilevskiy <a.mogilevskiy@tinkoff.ru>
2020-11-06 18:43:22 +0300

    feat (go-db_zip-parsing) update testing scripts

--- projects/go-db/test_scripts/game/update_game_archive.sh
+++ projects/go-db/test_scripts/game/update_game_archive.sh
@@ -2,10 +2,17 @@
 FPATH1="/Users/a.mogilevskiy/work/5gen/clean-city/archives/low.zip"
 FPATH2="/Users/a.mogilevskiy/work/5gen/clean-city/archives/medium.zip"
 
-URL="http://localhost:1234/api/v1/game/some-game-id"
+ARCHIVE_LOW1="archives=@/Users/a.mogilevskiy/work/5gen/clean-city/archives/low1.zip"
+ARCHIVE_MEDIUM="archives=@/Users/a.mogilevskiy/work/5gen/clean-city/archives/medium.zip"
+
+GAME_ID1="be359a94-d2cf-4ec4-8dc2-fbd8350c89ec" # past
+GAME_ID2="2bc4c2aa-aa87-406c-b922-334a95fed451" # future
+
+URL="http://localhost:1234/api/v1/game/${GAME_ID2}"
 
 # -F 'archives=@/Users/a.mogilevskiy/work/5gen/clean-city/archives/medium.zip' \
+# -F "$ARCHIVE_MEDIUM"
 
 curl \
-    -F 'archives=@/Users/a.mogilevskiy/work/5gen/clean-city/archives/low1.zip' \
+    -F "$ARCHIVE_LOW1" \
     -X PUT "$URL"
