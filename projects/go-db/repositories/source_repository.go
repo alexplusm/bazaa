f656e5f10e3315bce6348c7f8b528341ac12d553
A.Mogilevskiy <a.mogilevskiy@tinkoff.ru>
2020-11-09 12:48:41 +0300

    feat (go-db_zip-parsing) source repo

--- projects/go-db/repositories/source_repository.go
+++ projects/go-db/repositories/source_repository.go
@@ -1,6 +1,9 @@
 package repositories
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/Alexplusm/bazaa/projects/go-db/interfaces"
 	"github.com/Alexplusm/bazaa/projects/go-db/objects/dao"
 )
@@ -10,10 +13,39 @@ type SourceRepository struct {
 }
 
 const (
-	insertSourceStatement = ``
+	insertSourceStatement = `
+INSERT INTO sources ("game_id", "source_type", "created_at")
+VALUES ($1, $2, $3)
+RETURNING "source_id";
+`
+	// TODO: for schedule
+	insertSourceWithExistingIDStatement = `
+INSERT INTO sources ("game_id", "source_id", "source_type", "created_at")
+VALUES ($1, $2, $3, $4)
+RETURNING "source_id";
+`
 )
 
 func (repo *SourceRepository) InsertSource(source dao.SourceDAO) (string, error) {
-	sourceID := ""
+	p := repo.DBConn.GetPool()
+	conn, err := p.Acquire(context.Background())
+	if err != nil {
+		return "", fmt.Errorf("insert source: acquire connection: %v", err)
+	}
+	defer conn.Release()
+
+	row := conn.QueryRow(
+		context.Background(),
+		insertSourceStatement,
+		source.GameID, source.Type, source.CreatedAt,
+	)
+
+	var sourceID string
+
+	err = row.Scan(&sourceID)
+	if err != nil {
+		return "", fmt.Errorf("insert source: query: %v", err)
+	}
+
 	return sourceID, nil
 }
