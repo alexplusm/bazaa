7887a68fa640b5ad9bc4fc50e6bcd168d1504eb3
Alex Mogilevskiy <dark.qvark@gmail.com>
2020-11-13 11:21:27 +0300

    feat (go-db_queue) update sql schema

--- projects/go-db/sql/queries.sql
+++ projects/go-db/sql/queries.sql
@@ -5,6 +5,10 @@ GRANT ALL PRIVILEGES ON DATABASE godb0 TO alex;
 -- postgres://{user}:{password}@{hostname}:{port}/{database-name}
 -- postgres://postgres:postgres@localhost:5432/testik
 
+-- INFO: show all database tables
+SELECT table_name FROM information_schema.tables
+WHERE table_schema = 'public';
+
 -- INFO: uuid extension
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
@@ -13,9 +17,10 @@ SELECT uuid_generate_v1();
 
 -- games
 -- create game
-INSERT INTO games ("name", "start_date", "end_date", "answer_type", "question", "options_csv")
+INSERT INTO games ("ext_system_id", "name", "start_date", "end_date", "answer_type", "question", "options_csv")
 VALUES
 (
+    'ext-system-id-123',
     'first game!',
     '1601510400000',
     '1601683200000',
@@ -24,46 +29,43 @@ VALUES
     'Да,Нет'
 )
 RETURNING "game_id";
--- end games
+-- ##############################################
+
+-- ext_systems
+INSERT INTO external_systems (external_system_id)
+VALUES ('ex-1');
+-- ##############################################
 
 -- sources
--- INSERT schedule (schedule has own ID)
+-- insert schedule (schedule has own ID)
 INSERT INTO sources ("source_id", "source_type", "created_at", "game_id")
 VALUES ('some-source-id', '1', '1601683200000', 'some-game-id')
 RETURNING "source_id";
--- INSERT zip-archive (generate ID for archive source)
+
+-- insert zip-archive (generate ID for archive source)
 INSERT INTO sources ("source_type", "created_at", "game_id")
 VALUES ('1', '1601683200000', 'some-game-id')
 RETURNING "source_id";
--- end sources
+-- ##############################################
 
--- external_systems
-INSERT INTO external_systems (external_system_id)
-VALUES ('ex-1');
--- external_systems end
+-- screenshots
+-- insert without expert answer
+INSERT INTO screenshots ("game_id", "source_id", "filename")
+VALUES ("some-game-id", "some-source-id", "filename.jpg");
+
+-- insert with expert answer
+INSERT INTO screenshots ("game_id", "source_id", "filename", "expert_answer")
+VALUES ("some-game-id", "some-source-id", "filename.jpg", "1");
+-- ##############################################
 
 -- users
 INSERT INTO users (user_id)
 VALUES ('vasya_pup123');
--- users end
-
--- screenshots
-INSERT INTO screenshots ("game_id", "source_id", "filename")
-VALUES ("some-game-id", "some-source-id", "filename.jpg");
--- INFO: insert with expert answer
-INSERT INTO screenshots ("game_id", "source_id", "filename", "expert_answer")
-VALUES ("some-game-id", "some-source-id", "filename.jpg", "1");
--- screenshots end
+-- ##############################################
 
 -- TODO: into docs
 --     ExternalSystemId
 -- Date1
 -- Date2
 --
--- У кого больше правильный ответов - тот и лидер
---
--- Select game_id FROM games
--- WHERE games.start_date >= Date1 OR games.start_date <= Date2
---     INNER JOIN answers
--- ON answers.game_id = game_id
--- WHERE answers.external_system_id = ExternalSystemId
+
