5b14e39ceba185f11340dae53655ebd47c4137f6
Alex Mogilevskiy <dark.qvark@gmail.com>
2020-11-21 00:14:27 +0300

    feat (go-db_nginx) test request from web client

--- projects/go-db/web-client/src/App.vue
+++ projects/go-db/web-client/src/App.vue
@@ -1,28 +1,18 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    "hello world!"
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios"
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    mounted () {
+        axios
+            .get('/check/alive')
+            .then(response => console.log("response3333: ", response))
+    }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
