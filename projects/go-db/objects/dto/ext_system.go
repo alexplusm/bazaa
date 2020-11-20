ab6df7d58da6ee83044fd81d3a4a2c2d42094af5
Alex Mogilevskiy <dark.qvark@gmail.com>
2020-11-20 13:54:05 +0300

    test (go-db_queue) ext_system tests

--- projects/go-db/objects/dto/ext_system.go
+++ projects/go-db/objects/dto/ext_system.go
@@ -1,7 +1,11 @@
 package dto
 
 type CreateExtSystemRequestBody struct {
-	ID             string `json:"ext_system_id"`
+	ID             string `json:"extSystemId"`
 	Description    string `json:"description"`
-	PostResultsURL string `json:"post_results_url"`
+	PostResultsURL string `json:"postResultsUrl"`
+}
+
+type CreateExtSystemResponseBody struct {
+	ID string `json:"extSystemId"`
 }
