b12cc020e5c152351204d93a6fc2b2b252d772bb
Alex Mogilevskiy <dark.qvark@gmail.com>
2020-11-21 12:20:20 +0300

    refactor (s21-libft) memset.c

--- school21/42cursus/libft/ft_memset.c
+++ school21/42cursus/libft/ft_memset.c
@@ -7,8 +7,8 @@ void *tf_memset(void *b, int c, int len)
     //     ((int *)b)[i] = c;
     //     printf("i: %d | value: %c | char: %c \n", i, ((char *)b)[i], c);
     // }
-
     // return b;
+
     unsigned char *ptr = b;
     while (len-- > 0)
         *ptr++ = c;
--- school21/42cursus/libft/ft_memset_main.c
+++
@@ -1,3 +0,0 @@
-#include "ft_memset.c"
-
-// for testing
