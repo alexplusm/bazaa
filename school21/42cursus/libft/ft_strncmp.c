fd8d0b543551196f13408965a6e883fb16ef3545
Alex Mogilevskiy <dark.qvark@gmail.com>
2020-11-25 13:52:18 +0300

    fix (s21-libft) ft_strncmp

--- school21/42cursus/libft/ft_strncmp.c
+++ school21/42cursus/libft/ft_strncmp.c
@@ -2,10 +2,18 @@
 
 int ft_strncmp(const char *s1, const char *s2, ft_size_t n)
 {
-    ft_size_t i;
+    ft_size_t       i;
+    unsigned char   *ptr_1;
+    unsigned char   *ptr_2;
 
     i = 0;
-    while (s1[i] == s2[i] && (i + 1) < n)
+    ptr_1 = (unsigned char *) s1;
+    ptr_2 = (unsigned char *) s2;
+    while ((ptr_1[i] != '\0' || ptr_2[i] != '\0') && (i) < n)
+    {
+        if (ptr_1[i] != ptr_2[i])
+            return (ptr_1[i] - ptr_2[i]);
         i++;
-    return s1[i] - s2[i];
+    }
+    return (0);
 }
