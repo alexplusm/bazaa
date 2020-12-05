125406699788b1d5853bb1221900e5f2b7002018
Alex Mogilevskiy <dark.qvark@gmail.com>
2020-12-05 12:23:37 +0300

    feat (go-db_nginx) front: menu bar

---
+++ projects/go-db/web_client/src/components/MenuBar.vue
@@ -0,0 +1,16 @@
+<template>
+	<div>
+		<router-link to="/games">Games</router-link>
+		<br>
+		<router-link to="/game">Игра</router-link>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "MenuBar"
+}
+</script>
+
+<style scoped>
+</style>
