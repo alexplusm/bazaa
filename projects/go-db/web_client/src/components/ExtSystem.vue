e900acdba33088ae32a4f896aba18e67317bb1e4
Alex Mogilevskiy <dark.qvark@gmail.com>
2020-12-09 00:32:23 +0300

    feat (go-db_nginx) ExtSystem.vue

--- projects/go-db/web_client/src/components/ExtSystem.vue
+++ projects/go-db/web_client/src/components/ExtSystem.vue
@@ -1,16 +1,24 @@
 <template>
-	<div>
-		<ExtSystemSelect />
-
-	</div>
+	<v-container>
+		<v-row>
+			<v-col cols="6">
+				<ExtSystemSelect />
+			</v-col>
+			<v-col cols="6">
+				<ExtSystemCreate />
+			</v-col>
+		</v-row>
+	</v-container>
 </template>
 
 <script>
 import ExtSystemSelect from "./ExtSystemSelect";
+import ExtSystemCreate from "./ExtSystemCreate";
 
 export default {
 	name: "ExtSystem",
 	components: {
+		ExtSystemCreate,
 		ExtSystemSelect
 	}
 }
---
+++ projects/go-db/web_client/src/components/ExtSystemCreate.vue
@@ -0,0 +1,45 @@
+<template>
+	<section>
+		<v-form>
+			<v-text-field
+				label="ID"
+				v-model="extSystemId"
+			></v-text-field>
+
+			<v-text-field
+				label="Description"
+				v-model="description"
+				:rules="fieldRules"
+				required
+			></v-text-field>
+
+			<v-text-field
+				label="Post Results Url"
+				v-model="postResultsUrl"
+				:rules="fieldRules"
+				required
+			></v-text-field>
+
+		</v-form>
+	</section>
+</template>
+
+<script>
+import {fieldRequiredFunc} from "../utils/formUtils";
+
+export default {
+	name: "ExtSystemCreate",
+	data() {
+		return {
+			extSystemId: '',
+			description: '',
+			postResultsUrl: '',
+			fieldRules: [fieldRequiredFunc]
+		}
+	}
+}
+</script>
+
+<style scoped>
+
+</style>
