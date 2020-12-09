2a11b67ce9f80967700c8cf8ae7e23a7d74ba498
Alex Mogilevskiy <dark.qvark@gmail.com>
2020-12-09 10:15:46 +0300

    feat (go-db_nginx) AuthForm.vue

--- projects/go-db/web_client/src/components/auth/AuthForm.vue
+++ projects/go-db/web_client/src/components/auth/AuthForm.vue
@@ -1,14 +1,69 @@
 <template>
-	<section>
+	<v-form v-model="valid">
+		<v-text-field
+			label="Login"
+			required
+			v-model="login"
+			:rules="fieldRules"
+		></v-text-field>
 
-	</section>
+		<v-text-field
+			label="Password"
+			required
+			v-model="password"
+			:rules="fieldRules"
+		></v-text-field>
+
+		<v-row justify="center">
+			<v-progress-circular
+				v-if="loading"
+				indeterminate
+				color="primary"
+			>
+			</v-progress-circular>
+			<v-btn
+				v-else
+				:disabled="!valid"
+				color="success"
+				@click="submit"
+			>
+				Log in
+			</v-btn>
+		</v-row>
+	</v-form>
 </template>
 
 <script>
-// TODO: move form
+import {mapMutations} from "vuex";
+import {fieldRequiredFunc} from "../../utils/form-utils";
+
+const timeout = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 export default {
-	name: "AuthForm"
+	name: "AuthForm",
+	data: () => ({
+		valid: false,
+		loading: false,
+		login: 'a',
+		password: 'b',
+		fieldRules: [fieldRequiredFunc],
+	}),
+	methods: {
+		...mapMutations(['authorize']),
+		submit() {
+			if (!this.valid) {
+				return;
+			}
+
+			this.loading = true;
+			timeout(3)
+				.then(() => {
+					this.loading = false;
+					this.authorize();
+					this.$router.push('home');
+				});
+		},
+	}
 }
 </script>
 
--- projects/go-db/web_client/src/pages/AuthPage.vue
+++ projects/go-db/web_client/src/pages/AuthPage.vue
@@ -1,92 +1,23 @@
 <template>
 	<v-container>
 		<v-row justify="center">
-
 			<v-col cols="3">
-
 				<v-row justify="center">
 					<h3>Админка</h3>
 				</v-row>
 
-				<!-- TODO: FORM Into other component -->
-				<v-form v-model="valid">
-
-					<v-text-field
-						label="Login"
-						required
-						v-model="login"
-						:rules="loginRules"
-					></v-text-field>
-
-					<v-text-field
-						label="Password"
-						required
-						v-model="password"
-						:rules="passwordRules"
-					></v-text-field>
-
-					<v-row justify="center">
-						<v-progress-circular
-							v-if="loading"
-							indeterminate
-							color="primary"
-						>
-						</v-progress-circular>
-						<v-btn
-							v-else
-							:disabled="!valid"
-							color="success"
-							@click="submit"
-						>
-							Log in
-						</v-btn>
-					</v-row>
-				</v-form>
+				<AuthForm />
 			</v-col>
 		</v-row>
 	</v-container>
 </template>
 
 <script>
-import {fieldRequiredFunc} from "../utils/form-utils";
-
-function timeout(ms) {
-	return new Promise(resolve => setTimeout(resolve, ms));
-}
+import AuthForm from '../components/auth/AuthForm'
 
 export default {
 	name: "AuthPage",
-	data: () => ({
-		valid: false,
-		loading: false,
-		login: 'a',
-		password: 'b',
-		loginRules: [fieldRequiredFunc],
-		passwordRules: [fieldRequiredFunc],
-	}),
-	methods: {
-		authorize() {
-			this.$store.commit('authorize');
-		},
-		submit() {
-			console.log("valid: ", this.valid)
-			if (!this.valid) {
-				return;
-			}
-
-			this.loading = true;
-
-			timeout(3)
-				.then(() => {
-					console.log(this.password, this.login)
-				})
-				.then(() => {
-					this.loading = false;
-					this.authorize();
-					this.$router.push('home');
-				});
-		},
-	}
+	components: {AuthForm},
 }
 </script>
 
