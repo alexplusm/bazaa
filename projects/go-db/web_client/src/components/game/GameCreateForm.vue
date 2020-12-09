2e54efa2937012fa9550277a1a1a9ff8de3bb01b
Alex Mogilevskiy <dark.qvark@gmail.com>
2020-12-09 10:25:42 +0300

    feat (go-db_nginx) init game components

---
+++ projects/go-db/web_client/src/components/game/GameCreateForm.vue
@@ -0,0 +1,12 @@
+<template>
+
+</template>
+
+<script>
+export default {
+	name: "GameCreateForm",
+}
+</script>
+
+<style scoped>
+</style>
---
+++ projects/go-db/web_client/src/components/game/GameDetails.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+	name: "GameDetails",
+}
+</script>
+
+<style scoped>
+
+</style>
