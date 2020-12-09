6bed5eef75ded98cceadb61606fbd145070c0f75
Alex Mogilevskiy <dark.qvark@gmail.com>
2020-12-09 09:20:12 +0300

    refactor (go-db_nginx) game table

--- projects/go-db/web_client/src/components/game/GameList.vue
+++ projects/go-db/web_client/src/components/game/GameList.vue
@@ -1,7 +1,33 @@
 <template>
-	<section>
-		{{ this.games }}
-	</section>
+	<v-simple-table>
+		<template v-slot:default>
+			<thead>
+			<tr>
+				<th class="text-left">ID</th>
+				<th class="text-left">Name</th>
+				<th class="text-left">Status</th>
+				<th class="text-left">From</th>
+				<th class="text-left">To</th>
+			</tr>
+			</thead>
+			<tbody>
+			<tr
+				v-for="item in games"
+				:key="item.gameId"
+			>
+				<td>
+					<router-link :to="{ path: item.gameId }" append>
+						{{ item.gameId }}
+					</router-link>
+				</td>
+				<td>{{ item.name }}</td>
+				<td>{{ item.status }}</td>
+				<td>{{ item.from }}</td>
+				<td>{{ item.to }}</td>
+			</tr>
+			</tbody>
+		</template>
+	</v-simple-table>
 </template>
 
 <script>
