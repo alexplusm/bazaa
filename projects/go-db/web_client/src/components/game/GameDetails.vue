799473c9b8d659253c358a08c85c5d15b8061c8b
Alex Mogilevskiy <dark.qvark@gmail.com>
2020-12-09 12:27:13 +0300

    feat (go-db_nginx) game details

--- projects/go-db/web_client/src/components/game/GameDetails.vue
+++ projects/go-db/web_client/src/components/game/GameDetails.vue
@@ -9,7 +9,34 @@
 		</v-progress-circular>
 
 		<div v-else>
-			{{ this.currentGame }}
+			<div>Start date: {{ this.currentGame.startDate }}</div>
+			<div>Finish date: {{ this.currentGame.finishDate }}</div>
+			<div>Answer type: {{ this.currentGame.question.answerType }}</div>
+			<div>Question text: {{ this.currentGame.question.text }}</div>
+			<div>
+				Options
+				<div
+					v-for="option in this.currentGame.question.options"
+					:key="option.option"
+				>
+					{{ option.option }} | {{ option. text }}
+				</div>
+			</div>
+
+			<div>
+				Sources
+				<div
+					v-for="source in this.currentGame.sources"
+					:key="source.sourceId"
+				>
+					{{ source.sourceId }} | {{ source.type }}
+				</div>
+			</div>
+
+			<hr>
+
+			AddSource (check box: true - archive | false - schedule)
+			<GameUploadFile />
 		</div>
 	</section>
 
@@ -17,9 +44,11 @@
 
 <script>
 import {mapActions, mapGetters} from 'vuex';
+import GameUploadFile from "./GameUploadFile";
 
 export default {
 	name: "GameDetails",
+	components: {GameUploadFile},
 	data() {
 		return {
 			loading: true,
---
+++ projects/go-db/web_client/src/components/game/GameUploadFile.vue
@@ -0,0 +1,14 @@
+<template>
+	<section>
+		UploadFile
+	</section>
+</template>
+
+<script>
+export default {
+	name: "GameUploadFile"
+}
+</script>
+
+<style scoped>
+</style>
