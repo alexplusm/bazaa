bb7b385afa5821eab05cdae0f133188eea340428
Alex Mogilevskiy <dark.qvark@gmail.com>
2020-12-10 14:39:35 +0300

    feat (go-db_nginx) domain consts

---
+++ projects/go-db/web_client/src/domain/consts.js
@@ -0,0 +1,34 @@
+// INFO: sync with backend
+const textType = {
+    value: 1,
+    text: 'Текстовый',
+};
+
+const categoryType = {
+    value: 2,
+    text: 'Категориальный',
+};
+
+const rectangleCoordsType = {
+    value: 3,
+    text: 'Координаты прямоугольника',
+};
+
+const polygonCoordsType = {
+    value: 4,
+    text: 'Координаты точек полигонов',
+}
+
+export const answerTypesMap = {
+    textType,
+    categoryType,
+    rectangleCoordsType,
+    polygonCoordsType,
+}
+
+export const answerTypesArray = [
+    textType,
+    categoryType,
+    rectangleCoordsType,
+    polygonCoordsType
+];
