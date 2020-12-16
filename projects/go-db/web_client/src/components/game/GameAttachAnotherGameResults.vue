20679fcb8f6842c24820d295cc328dc2575fd9d7
Alex Mogilevskiy <dark.qvark@gmail.com>
2020-12-16 17:22:10 +0300

    feat (go_db-another-game) game attach another game results component

---
+++ projects/go-db/web_client/src/components/game/GameAttachAnotherGameResults.vue
@@ -0,0 +1,30 @@
+<template>
+	<section>
+		{{ currentGames }}
+	</section>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex';
+
+export default {
+	name: "GameAttachAnotherGameResults",
+	data: () => ({
+		loading: true,
+	}),
+	computed: {
+		...mapGetters(['games']),
+		currentGames() {
+			console.log("GAMES !!!");
+			return this.games;
+		}
+	},
+	methods: {
+		...mapActions(['getGameList']),
+	},
+	mounted() {
+		this.getGameList().finally(() => this.loading = false)
+
+	}
+}
+</script>
