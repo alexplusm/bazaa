174f121cd53a67f3484452f851d0fc21e0a7dcd1
A. Mogilevskiy <dark.qvark@gmail.com>
2020-12-23 14:02:25 +0300

    feat (go_db-another-game) log AttachArchives (8)

--- projects/go-db/cmd/kek.go
+++ projects/go-db/cmd/kek.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	serv := services.ValidateFacesService{}
-	img := "0-DVN_b_SVAO_541_1-04_08_2020_13_00_30.jpg"
+	img := "0-DVN_SAO_1094_0-07_08_2020_13_10_30.jpg"
 	a, b := serv.Validate("media_root/" + img)
 	fmt.Println(a, b)
 }
